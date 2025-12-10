9a3677c708511011f7f96005b28749b8edd4f916
ness580 <nessrineamri7@gmail.com>
2025-12-10 23:06:02 +0100

    Correction pom.xml et Jenkinsfile, tests ignorés pour pipeline

--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -1,10 +1,18 @@
 package tn.esprit.spring.services;
 
-
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
-class UserServiceImplTest {}
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserServiceImplTest {
+
+    @Test
+    @Order(1)
+    void contextLoads() {
+        // test minimal pour vérifier que Spring Boot démarre
+    }
+}
